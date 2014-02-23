fb3c86fd19724f3aa3fdb038772706cd5ccaf631
varun kumar <bit.varun@gmail.com>
2014-02-23 13:07:32 -0800

    Addin backtracking

--- Limo/src/com/thinklazy/SubSetSum.java
+++ Limo/src/com/thinklazy/SubSetSum.java
@@ -14,7 +14,8 @@ public class SubSetSum {
 
 	private static boolean isSubsetSum(int[] set, int sum) {
 		 int n =  set.length;
-		 boolean[][] hasSubSet = new boolean[sum+1][n+1]; //A[i][j] => true if A[0...j] has subset of sum i;
+		 boolean[][] hasSubSet = new boolean[sum+1][n+1]; 
+		 //A[i][j] => true if A[0...j] has subset of sum i;
 		
 		 for(int i = 0; i <= n; i++) {
 			 hasSubSet[0][i] = true;
